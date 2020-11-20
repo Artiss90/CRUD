69aa44c1e0320fa245678ab2ea6439bf0d516a86
Artem <64410376+Artiss90@users.noreply.github.com>
2020-11-20 14:05:35 +0200

    PUT, PUTCH

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import './js/c';
+// import './js/c';
 // import './js/r';
-// import './js/u';
+import './js/u';
 // import './js/d';
--- src/js/u.js
+++ src/js/u.js
@@ -1,1 +1,22 @@
 import BASE_URL from './url';
+
+const myBook = {
+    "title": "Two tower"
+};
+
+
+
+function changeBooks(update, id){
+
+    const options = {
+        method: 'PATCH',
+        headers: {"Content-Type": 'application/json',},
+        body: JSON.stringify(update),
+    };
+
+   return fetch(`${BASE_URL}/${id}`, options).then(response => response.json()).then(data => console.log(data))
+    };
+
+// changeBooks(myBook,25)
+
+// changeBooks({"rating": 8}, 26)
