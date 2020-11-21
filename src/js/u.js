c5f5dfa3767e519e577b20bff2476adb71391f4a
Artem <64410376+Artiss90@users.noreply.github.com>
2020-11-21 14:15:40 +0200

    promise => ASYNC

--- src/js/u.js
+++ src/js/u.js
@@ -17,6 +17,23 @@ function changeBooks(update, id){
    return fetch(`${BASE_URL}/${id}`, options).then(response => response.json()).then(data => console.log(data))
     };
 
+async function asyncChangeBooks(update,id){
+    const options = {
+        method: 'PATCH',
+        headers: {"Content-Type": 'application/json',},
+        body: JSON.stringify(update),
+    }
+
+    try {
+    const fetchBook = await fetch(`${BASE_URL}/${id}`, options);
+    const data = await fetchBook.json();
+    return console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}    
+
 // changeBooks(myBook,25)
 
-// changeBooks({"rating": 8}, 26)
+// changeBooks({"rating": 7}, 26)
+asyncChangeBooks({"rating": 5}, 26)
